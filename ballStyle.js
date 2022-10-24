0b4748add9de57243933721bc3168055f3b4ee02
norman - g4billiards <46265156+ngholson@users.noreply.github.com>
2022-10-23 19:10:30 -0500

    Update ballStyle.js

--- ballStyle.js
+++ ballStyle.js
@@ -2,3 +2,8 @@ var blackWhite = "black";
 // Ball color setting
 // "black" = Aramith Black Tournament Balls
 // "white" = Standard Ball Colors
+// 
+// you can make your own, 
+// create a new folder inside the images folder
+// same your images numbered 1-15 as PNG
+// and change this to the new folder name. 
