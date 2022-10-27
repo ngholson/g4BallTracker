02c0dd3c06eff6463fa63b6bf0500457a1063f3a
norman - g4billiards <46265156+ngholson@users.noreply.github.com>
2022-10-26 20:49:56 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-var blackWhite = "black";		
 // Ball color setting
 // "black" = TV Tournament ball colors with black stripe
 // "white" = TV Tournament ball colors with white stripe
@@ -8,3 +7,10 @@ var blackWhite = "black";
 // create a new folder inside the images folder
 // same your images numbered 1-15 as PNG
 // and change this to the new folder name. 
+var blackWhite = "black";	
+
+
+// changes ball alignment on stream output
+var position = "horizontal";		// options are "vertical" or "horizontal"
+var leftRight = "left";			// left or right (applies to vertical position)
+
